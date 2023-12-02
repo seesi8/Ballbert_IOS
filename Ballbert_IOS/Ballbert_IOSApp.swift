f0ac715e42ec248105db849d846f546f292ad128
Samuel Liebert <ballbertllc@gmail.com>
2023-12-01 18:03:40 -0600

    Initial Commit

---
+++ Ballbert_IOS/Ballbert_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ballbert_IOSApp.swift
+//  Ballbert_IOS
+//
+//  Created by Sam Liebert on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Ballbert_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ballbert_IOS/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Ballbert_IOS
+//
+//  Created by Sam Liebert on 12/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
